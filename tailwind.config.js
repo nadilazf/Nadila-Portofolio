97298eb879817723036c3847b032dc36cf5d00fd
nadilazaf <nadilazarifani506@gmail.com>
2024-04-27 15:28:16 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'brown': '#3E3232',
+        'b2': '#B89C9C',
+        'b3': '#7E6363',
+      }
+    },
+  },
+  plugins: [],
+}
+
