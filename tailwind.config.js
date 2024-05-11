b379265a5d95616a9c2f94d076a41a0932407e98
nadilazaf <nadilazarifani506@gmail.com>
2024-05-11 16:49:04 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         'brown': '#3E3232',
         'b2': '#B89C9C',
         'b3': '#7E6363',
+      },
+      backgroundImage: {
+        'mobile': "url('/src/img/mobile.png')",
+        'laptop': "url('/src/img/homebg.png')",
       }
     },
   },
